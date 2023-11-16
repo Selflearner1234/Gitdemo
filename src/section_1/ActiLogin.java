f693901683d5689ebb847e5dbcb524caac962b48
narendra <selflearner1234@outlook.com>
2023-11-16 05:41:57 +0530

    new code is added to this file

--- src/section_1/ActiLogin.java
+++ src/section_1/ActiLogin.java
@@ -13,7 +13,13 @@ public class ActiLogin {
 		driver.findElement(By.name("pwd")).sendKeys("manager");
 		driver.findElement(By.id("loginButton")).click();
 
-	
+//		ChromeDriver driver = new ChromeDriver();
+//		driver.manage().window().maximize();
+//		driver.get("https://demo.actitime.com/login.do");
+//		driver.findElement(By.name("username")).sendKeys("admin");
+//		driver.findElement(By.name("pwd")).sendKeys("manager");
+//		driver.findElement(By.id("loginButton")).click();
+
 		
 
 	}
